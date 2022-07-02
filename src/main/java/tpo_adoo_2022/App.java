9bfbca15d68fa4e8daad8d8dba0ce8ca3eaa40bf
Diego Sanchez <diegoasp18@gmail.com>
2022-07-02 18:32:22 -0300

    First commit

---
+++ src/main/java/tpo_adoo_2022/App.java
@@ -0,0 +1,13 @@
+package tpo_adoo_2022;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
